f91f9b813ecb3081f6d1ac253fcfab7a039fee24
Victor Brito de Sousa <victorbrito001@gmail.com>
2022-03-11 09:36:46 -0300

    Chapter II - Iniciando a API

---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+
+import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationsRepository";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
+
+const specificationsRoutes = Router();
+
+const specificationsRepository = new SpecificationsRepository();
+
+specificationsRoutes.post("/", (request, response) => {
+  return createSpecificationController.handle(request, response);
+});
+
+specificationsRoutes.get("/", (request, response) => {
+  const all = specificationsRepository.list();
+
+  return response.json(all);
+});
+
+export { specificationsRoutes };
