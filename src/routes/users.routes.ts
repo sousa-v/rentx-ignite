2887d821f0b1581f17da91fb740dcb2ac625eca4
Victor Sousa <victorbrito001@gmail.com>
2022-03-22 11:58:36 -0300

    Adicionando coluna de avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
 const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
+const updateUserAvatarUseCase = new UpdateUserAvatarController();
 
 usersRoutes.post("/", createUserController.handle);
+usersRoutes.patch("/avatar", updateUserAvatarUseCase.handle);
 
 export { usersRoutes };
