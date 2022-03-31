53fd7418c9b1e54cf620a30c996d49621bb97ade
Victor Sousa <victorbrito001@gmail.com>
2022-03-31 13:32:36 -0300

    Criando migrations do aluguel

--- src/shared/infra/typeorm/migrations/1647517818452-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1647517818452-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1647517818452 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
