31da1a45b85592b5d8105917459149e1aa2213ec
Victor Sousa <victorbrito001@gmail.com>
2022-04-06 14:15:54 -0300

    Controller refresh token

--- src/modules/accounts/useCases/refreshToken/RefreshTokenController.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenController.ts
@@ -5,10 +5,16 @@ import { RefreshTokenUseCase } from "./RefreshTokenUseCase";
 
 class RefreshTokenController {
   async handle(request: Request, response: Response): Promise<Response> {
+    const token =
+      request.body.token ||
+      request.headers["x-access-token"] ||
+      request.query.token;
+
     const refreshtokenUseCase = container.resolve(RefreshTokenUseCase);
 
-    const token = await refreshtokenUseCase.execute();
-    return response.send();
+    const refresh_token = await refreshtokenUseCase.execute(token);
+
+    return response.json(refresh_token);
   }
 }
 
