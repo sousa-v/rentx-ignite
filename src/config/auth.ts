c32bb2f4ad7839fb37989a3d5674351cc9a9f4b3
Victor Sousa <victorbrito001@gmail.com>
2022-04-06 13:27:36 -0300

    Refatorando autenticacao do usuario

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "nodejs",
+  expires_in_token: "15m",
+  secret_refresh_token: "nodejs2",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
